88c1614132c756dc1be77e8455cabc2b0bc3add7
yuli <275452142@qq.com>
2021-08-16 00:03:14 +0800

    副作用

---
+++ src/main/scala/function/localEffects/LocalEffects.scala
@@ -0,0 +1,82 @@
+package function.localEffects
+
+class LocalEffects {
+
+}
+
+sealed trait ST[S, A] {
+  self =>
+  protected def run(s: S): (A, S)
+
+  def map[B](f: A => B): ST[S, B] = new ST[S, B] {
+    override protected def run(s: S): (B, S) = {
+      val (a, s1) = self.run(s)
+      (f(a), s1)
+    }
+  }
+
+  def flatMap[B](f: A => ST[S, B]): ST[S, B] = new ST[S, B] {
+    override protected def run(s: S): (B, S) = {
+      val (a, s1) = self.run(s)
+      f(a).run(s1)
+    }
+  }
+}
+
+object ST {
+  def apply[S, A](a: => A) = {
+    lazy val memo = a
+    new ST[S, A] {
+      override protected def run(s: S): (A, S) = (memo, s)
+    }
+  }
+
+  def runST[A](st: RunableST[A]): A = st[Unit].run(())._1
+}
+
+sealed trait STRef[S, A] {
+  protected var cell: A
+
+  def read: ST[S, A] = ST(cell)
+
+  def write(a: A): ST[S, Unit] = new ST[S, Unit] {
+    override protected def run(s: S): (Unit, S) = {
+      cell = a
+      ((), s)
+    }
+  }
+}
+
+object STRef {
+  def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef[S, A] {
+    var cell = a
+  })
+}
+
+trait RunableST[A] {
+  def apply[S]: ST[S, A]
+}
+
+sealed abstract class STArray[S, A](implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.size)
+
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+    def run(s: S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def freeze: ST[S, List[A]] = ST(value.toList)
+}
+
+object STArray {
+  def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A]() {
+      lazy val value = Array.fill(sz)(v)
+    })
+}
